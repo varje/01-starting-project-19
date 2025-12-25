24233921225b2062a8bd10f4491288ea07127420
varje <varjekivila@gmail.com>
2025-12-25 15:54:31 +0200

    use generic type

--- basics.ts
+++ basics.ts
@@ -29,3 +29,12 @@ function add(a: number, b: number) {
 function output(value: any) {
   console.log(value);
 }
+
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const demoArray = [1, 2, 3];
+const updatedArray = insertAtBeginning(demoArray, -1);
+const stringArray = insertAtBeginning(['a', 'b', 'c'], 'd');
