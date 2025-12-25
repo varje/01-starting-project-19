378907cb2fd92921b387f065895f15ada119ae07
varje <varjekivila@gmail.com>
2025-12-25 15:30:36 +0200

    add base types example

--- basics.ts
+++ basics.ts
@@ -3,3 +3,13 @@ let userName: string;
 userName = 'Max';
 let isInstructor: boolean;
 isInstructor = true;
+let hobbies: string[];
+hobbies = ['Sports', 'Cooking'];
+let person: { name: string; age: number };
+person = {
+  name: 'Max',
+  age: 32,
+};
+
+let people: { name: string; age: number }[];
+people = [person];
